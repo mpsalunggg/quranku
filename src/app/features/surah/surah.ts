ea058bfebd402ca73dae1ff93dc0fd11abd486c8
mpsalunggg <putrasatria893@gmail.com>
2025-09-21 19:03:39 +0700

    chore: change to reactive state

--- src/app/features/surah/surah.ts
+++ src/app/features/surah/surah.ts
@@ -1,35 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuranService, Surah as SurahModel } from '../../services/quran.service';
 
 @Component({
   selector: 'app-surah',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './surah.html',
 })
 export class Surah {
-  surahs: SurahModel[] = [];
-  loading = false;
-  error: string | null = null;
+  surahs = signal<SurahModel[]>([]);
+  loading = signal<boolean>(false);
+  error = signal<string | null>(null);
 
-  constructor(private quranService: QuranService) {}
-
-  ngOnInit() {
+  constructor(private quranService: QuranService) {
     this.loadSurahs();
   }
 
   loadSurahs() {
-    this.loading = true;
-    this.error = null;
+    this.loading.set(true);
+    this.error.set(null);
 
     this.quranService.getSurahs().subscribe({
       next: (response) => {
-        this.surahs = response.data;
-        this.loading = false;
+        console.log("res" , response)
+        this.surahs.set(response.data);
+        this.loading.set(false);
       },
-      error: () => {
-        this.error = 'Failed to load surahs';
-        this.loading = false;
+      error: (err) => {
+        this.error.set('Failed to load surahs');
+        this.loading.set(false);
       },
     });
   }
--- src/app/shared/layout/layout.ts
+++ src/app/shared/layout/layout.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Button } from '../components/button/button';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, Button],
+  imports: [CommonModule, RouterModule],
   templateUrl: './layout.html',
 })
 export class Layout {}
