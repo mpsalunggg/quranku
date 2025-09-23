2d3f14cf34e3483e32be55c7ebbfca211dc95084
mpsalunggg <putrasatria893@gmail.com>
2025-09-23 16:02:58 +0700

    feat: add header typography surah

--- src/app/shared/components/card/card.ts
+++ src/app/shared/components/card/card.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { cn } from '../../utils/tw';
 
 @Component({
   selector: 'ui-card',
   imports: [],
   templateUrl: './card.html',
 })
-export class Card {}
+export class Card {
+  class = input<string>('');
+
+  cardClasses = computed((): string => {
+    const baseClasses = 'rounded-xl shadow-md border border-gray-200 p-4 bg-white';
+
+    return cn(baseClasses, this.class());
+  });
+}
