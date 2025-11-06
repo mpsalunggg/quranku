872096f82cacf5d8f778ef3eafa25665eba7abab
mpsalunggg <putrasatria893@gmail.com>
2025-11-06 15:15:31 +0700

    feat(service): adjust type get detail surah

--- src/app/services/quran.service.ts
+++ src/app/services/quran.service.ts
@@ -9,6 +9,32 @@ export interface Surah {
   englishNameTranslation: string;
   numberOfAyahs: number;
   revelationType: string;
+  ayahs?: Ayah[];
+  edition?: Edition;
+}
+
+export interface Ayah {
+  number: number;
+  audio: string;
+  audioSecondary: string[];
+  text: string;
+  numberInSurah: number;
+  juz: number;
+  manzil: number;
+  page: number;
+  ruku: number;
+  hizbQuarter: number;
+  sajda: boolean;
+}
+
+export interface Edition {
+  identifier: string;
+  language: string;
+  name: string;
+  englishName: string;
+  format: string;
+  type: string;
+  direction: string | null;
 }
 
 @Injectable({
@@ -23,7 +49,7 @@ export class QuranService {
     return this.http.get<{ data: Surah[] }>(`${this.baseUrl}/surah`);
   }
 
-  getAyah(ayahId: string, reciterEdition = 'ar.alafasy'): Observable<{ data: any }> {
-    return this.http.get<{ data: any }>(`${this.baseUrl}/surah/${ayahId}/${reciterEdition}`);
+  getAyah(ayahId: string, reciterEdition = 'ar.alafasy'): Observable<{ data: Surah }> {
+    return this.http.get<{ data: Surah }>(`${this.baseUrl}/surah/${ayahId}/${reciterEdition}`);
   }
 }
